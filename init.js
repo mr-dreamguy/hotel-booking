5fa6aa3a1f343453bdf7c2cd9d20b212a10ce96c
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-06-29 08:40:03 +0530

    Added readme

---
+++ init.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+const { data } = require('./data.js');
+
+async function connect() {
+    mongoose.connect('mongodb://localhost:27017/wanderlust');
+}
+
+connect()
+.then(res => console.log('connected to db'))
+.catch(err => console.log(err));
+
+async function initdb() {
+    await Listing.deleteMany({});
+    await Listing.insertMany(data);
+    console.log('db was initialised');
+}
+
+initdb();
---
+++ readme.md
@@ -0,0 +1,9 @@
+# hotel-booking
+
+This page is built with html, css, javascript
+Frameworks used are express, ejs, mongoose, ejs-mate, method-override
+
+To run the page download the files and run init.js for data in database
+Run app.js for the site visible at 'localhost:8080/listings'
+
+Softwares required are mongodb, node.js
