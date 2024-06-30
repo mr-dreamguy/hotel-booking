35a33c285c8ac27ef409e86029cba087c10905f2
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-06-30 23:12:24 +0530

    Made some changes and added validation for form in new route

--- models/listing.js
+++ models/listing.js
@@ -1,27 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title : String,
-    description : String,
-    image : {
-        filename : {
-            type : String,
-            default : 'listingimage',
-        },
-        url : {
-            type : String,
-            default : 'https://images.unsplash.com/photo-1719403278702-f1c375a2f55e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            set : (v) => v === "" 
-            ? 'https://images.unsplash.com/photo-1719403278702-f1c375a2f55e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            : v,
-        },
+  title: String,
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
     },
-    price : Number,
-    location : String,
-    country : String
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1719403278702-f1c375a2f55e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1719403278702-f1c375a2f55e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : v,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing = mongoose.model('Listing', listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 
 module.exports = Listing;
